15265b39575523f99ca395f05707fe6320c88b73
geeh.oliveira@hotmail.com <geeh.oliveira@hotmail.com>
2023-06-13 16:53:01 -0300

    ex

--- ProjetoWebClima/frontPage-Gesley/script.js
+++ ProjetoWebClima/frontPage-Gesley/script.js
@@ -1,4 +1,30 @@
-function getWeather(city) {
+apiKey = "5a6dbc367b74700ff55f22a655c45ddb"
+apiBDImagemUnsplash = "https://source.unsplash.com/random/?";
+apiBandeiraPais = "https://flagsapi.com/"
+tempElement = document.querySelector("#temperatura");
+climaIconElement = document.querySelector("#clima-icon");
+dadosTemp = document.querySelector(".dadosTemperatura")
+dadosLog = document.querySelector(".dadosLogradouro")
 
-  
-}  
+document.querySelector("#verificarClima").addEventListener("click", async (e) => {
+    e.preventDefault();
+    dadosTemp.style.display = "block"
+    let cidade = document.querySelector("input[name='cidade']").value
+    const apiClimaURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`;
+    document.getElementById('imgCidade').setAttribute('src', apiBDImagemUnsplash+cidade);
+    const res = await fetch(apiClimaURL)
+    const data = await res.json()
+    if (data.cod === "404"){
+        alert("Cidade Inválida")
+        return
+    }
+    document.getElementById("imgPais").setAttribute("src", apiBandeiraPais+data.sys.country);
+    temperatura.innerText = parseFloat(data.main.temp)+ "º";
+    humidade.innerText = parseFloat(data.main.humidity)+ "%";
+    vento.innerText = parseFloat(data.wind.speed)+ "km/h" 
+    tempo.innerText = data.weather[0].description; 
+    document.getElementById("cidadeTemp").innerText = cidade;
+    document.getElementById("icone").setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`); 
+    document.getElementById("imgPais").setAttribute("src", apiBandeiraPais+data.sys.country+"/flat/64.png");
+    document.getElementById('imgCidade').setAttribute('src', apiBDImagemUnsplash+cidade);
+});
