0125020e0dc25edea4450202fb4c4dab83e5905e
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2023-06-15 21:53:28 -0300

    atualiza

--- ProjetoWebClima/artigos-joao-l/script.js
+++ ProjetoWebClima/artigos-joao-l/script.js
@@ -1,4 +1,2 @@
-function frescurinha(){
-    document.querySelector("#imagens").setAttribute('width')
-    
-}
+noticia = "https://newsdata.io/api/1/news?apikey=pub_24552fa2900973923bb0121f5e8d0e4932e2b&q=agronegocio&country=br"
+document.getElementById("not1").setAttribute("src", noticia) 
