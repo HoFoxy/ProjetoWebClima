3c9d280d634639fd8b7c83832f179882ad4c1eb4
geeh.oliveira@hotmail.com <geeh.oliveira@hotmail.com>
2023-06-18 19:11:55 -0300

    ex

--- ProjetoWebClima/frontPage-Gesley/script.js
+++ ProjetoWebClima/frontPage-Gesley/script.js
@@ -6,26 +6,30 @@ climaIconElement = document.querySelector("#clima-icon");
 dadosTemp = document.querySelector(".dadosTemperatura")
 dadosLog = document.querySelector(".dadosLogradouro")
 
-document.querySelector("#verificarClima").addEventListener("click", async (e) => {
+document.querySelector("#btnVerificarClima").addEventListener("click", async (e) => {
     e.preventDefault();
-    dadosTemp.style.display = "block"
-    let cidade = document.querySelector("input[name='cidade']").value
-    const apiClimaURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`;
-    document.getElementById('imgCidade').setAttribute('src', apiBDImagemUnsplash+cidade);
+    let cidade = document.querySelector("input[name='pesquisarCidade']").value
+    const apiClimaURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`
     const res = await fetch(apiClimaURL)
     const data = await res.json()
     if (data.cod === "404"){
         alert("Cidade Inválida")
         return
     }
-    document.getElementById("imgPais").setAttribute("src", apiBandeiraPais+data.sys.country);
-    temperatura.innerText = parseFloat(data.main.temp)+ "º";
-    humidade.innerText = parseFloat(data.main.humidity)+ "%";
-    vento.innerText = parseFloat(data.wind.speed)+ "km/h";
-    tempo.innerText = data.weather[0].description; 
-    nome = 
-    document.getElementById("cidadeTemp").innerText = cidade;
-    document.getElementById("icone").setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`); 
-    document.getElementById("imgPais").setAttribute("src", apiBandeiraPais+data.sys.country+"/flat/64.png");
-    document.getElementById('imgCidade').setAttribute('src', apiBDImagemUnsplash+cidade);
+    document.querySelector("#nomeCidade").innerHTML = cidade;
+    let temperatura = parseFloat(data.main.temp);
+    let temperaturaMin = parseFloat(data.main.temp_min);
+    let temperaturaMax = parseFloat(data.main.temp_max);
+    let umidade = parseFloat(data.main.humidity);
+    let vento = parseFloat(data.wind.speed);
+    let tempo = data.weather[0].description;
+    let pressao = data.main.pressure;
+    document.querySelector('#temperatura').innerHTML = `Temperatura: ${temperatura} º`
+    document.querySelector('#temperaturaMinMax').innerHTML = `Max: ${temperaturaMax} º / Mín: ${temperaturaMin} º`
+    document.querySelector('#umidade').innerHTML = `Umidade: ${umidade} %`
+    document.querySelector('#pressao').innerHTML = `Pressão: ${pressao} mb`
+    document.querySelector('#vento').innerHTML = `Vento: ${vento} km/h`
+    document.querySelector('#tempo').innerHTML = `Tempo: ${tempo}`
+    document.querySelector('#fotoClima').setAttribute('src', apiBDImagemUnsplash+tempo);
 });
+
