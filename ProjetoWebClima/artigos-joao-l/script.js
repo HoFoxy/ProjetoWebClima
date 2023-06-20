916c4c59567c5bb9210106f6c604ab12159bc9c2
GesleyOliveira <127444907+GesleyOliveira@users.noreply.github.com>
2023-06-19 21:03:54 -0300

    ex

--- ProjetoWebClima/artigos-joao-l/script.js
+++ ProjetoWebClima/artigos-joao-l/script.js
@@ -1,6 +1,5 @@
 async function noticias(){
-    apikey = "pub_246657934d0e54867d40fee9cf274d6809088"
-    apikey2 = "pub_24552fa2900973923bb0121f5e8d0e4932e2b"
+    apikey = "..."
     const noticiaapi = `https://newsdata.io/api/1/news?apikey=${apikey}&q=agronegocio&country=br`
     const noticia = await fetch(noticiaapi)
     const data = await noticia.json() 
